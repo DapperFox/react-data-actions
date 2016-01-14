6382b14191977411ef17e7ae2f6e75f03a5ca0b4
Blake Bishop <blake@nuvi.com>
2016-01-14 15:39:24 -0700

    Add support for ff and safari

--- src/helpers/headersFromResponse.js
+++ src/helpers/headersFromResponse.js
@@ -1,16 +1,29 @@
 import _ from 'lodash';
 export default function headersFromResponse (response) {
   const headers = {};
-  for (const pair of response.headers.entries()) {
-    const key = pair[0];
-    const value = pair[1];
-    if (headers[key]) {
-      if (!_.isArray(headers[key])) {
-        headers[key] = [headers[key]];
+  if (response.headers.entries) {
+    for (const pair of response.headers.entries()) {
+      const key = pair[0];
+      const value = pair[1];
+      if (headers[key]) {
+        if (!_.isArray(headers[key])) {
+          headers[key] = [headers[key]];
+        }
+        headers[key].push(value);
+      } else {
+        headers[key] = value;
       }
-      headers[key].push(value);
-    } else {
-      headers[key] = value;
+    }
+  } else if (response.headers.map) {
+    for (let i in response.headers.map) {
+      if (response.headers.map.hasOwnProperty(i)) {
+        const value = response.headers.map[i];
+        if (value.length > 1) {
+          headers[i] = value;
+        } else {
+          headers[i] = value[0];
+        }
+      }
     }
   }
   return headers;
