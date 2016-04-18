6dd158884ee6187a911aab9550ce9db1901db95d
Dennis Wilkins <wilkinsd@nuvi.com>
2016-04-18 10:56:16 -0600

    Even more actions generator tests

--- tests/restActionsGenerator.js
+++ tests/restActionsGenerator.js
@@ -17,4 +17,18 @@ describe('restActionsGenerator', function () {
     const indexActionTwo = gen.indexAction();
     expect(indexAction).toEqual(indexActionTwo);
   });
+
+  it('should have rest actions generated', function () {
+    const gen = restActionsGenerator({});
+    expect(gen.indexAction()).toBeA('function');
+    expect(gen.showAction()).toBeA('function');
+    expect(gen.createAction()).toBeA('function');
+    expect(gen.patchAction()).toBeA('function');
+    expect(gen.updateAction()).toBeA('function');
+    expect(gen.updateIndexAction()).toBeA('function');
+    expect(gen.appendIndexAction()).toBeA('function');
+    expect(gen.invalidateShowAction()).toBeA('function');
+    expect(gen.invalidateIndexAction()).toBeA('function');
+    expect(gen.invalidateAllAction()).toBeA('function');
+  });
 });
