8b9b004118a8230c497628987764a858eaf6d663
Dennis Wilkins <dennis@inlineblock.com>
2016-08-17 16:02:39 -0600

    Fixing some path creation stuff, to not add extension if its already there, to be able ot use the ID in the regex path, to not add the id if it was found in the regex

--- src/helpers/buildRequestPath.js
+++ src/helpers/buildRequestPath.js
@@ -1,11 +1,13 @@
 export default function buildRequestPath (options, modelId) {
   let urlPath = options.path || options.url;
+  let modelIdUsed = false;
   const idAttribute = options.idAttribute || 'id';
   urlPath = urlPath.replace(/:([a-zA-Z_]+)/g, (uncleanMatch) => {
     const match = uncleanMatch.substr(1);
     let found = options[match];
-    if (match === 'id' || match === idAttribute) {
-      found = options[idAttribute] || options.id;
+    if (found === undefined && (match === 'id' || match === idAttribute)) {
+      found = modelId || options[idAttribute] || options.id;
+      modelIdUsed = true;
     }
     if (found === undefined) {
       throw new Error(`url route regex failed to find match for ${match}`);
@@ -13,11 +15,11 @@ export default function buildRequestPath (options, modelId) {
     return found;
   });
 
-  if (modelId !== undefined) {
+  if (modelId !== undefined && !modelIdUsed) {
     urlPath = `${urlPath}/${modelId}`;
   }
 
-  if (options.extension) {
+  if (options.extension && urlPath.substr(urlPath.length - options.extension.length - 1) !== `.${options.extension}`) {
     urlPath = `${urlPath}.${options.extension}`;
   }
   return urlPath;
--- tests/helpers/buildRequestPath.js
+++ tests/helpers/buildRequestPath.js
@@ -26,6 +26,18 @@ describe('#buildRequestPath', function () {
     expect(buildRequestPath({path: 'hello'}, 13)).toEqual('hello/13');
   });
 
+  it('should not append modelId if match was found in path regex', () => {
+    expect(buildRequestPath({path: 'hello/:id/request'}, 13)).toEqual('hello/13/request');
+  });
+
+  it('should not append modelId if match was found in path regex with extension', () => {
+    expect(buildRequestPath({path: 'hello/:id/request.json', extension: 'json'}, 13)).toEqual('hello/13/request.json');
+  });
+  
+  it('should not append modelId if match was found in path regex with idAttribute', () => {
+    expect(buildRequestPath({path: 'hello/:guid/request', idAttribute: 'guid' }, 13)).toEqual('hello/13/request');
+  });
+
   it('should append slash and add model to path when supplied id with nested attribute', () => {
     expect(buildRequestPath({path: 'hello/:nest/children', nest: 15}, 13)).toEqual('hello/15/children/13');
   });
@@ -36,5 +48,9 @@ describe('#buildRequestPath', function () {
   
   it('should append extension given if provided with model id', () => {
     expect(buildRequestPath({path: 'hello', extension: 'json'}, 14)).toEqual('hello/14.json');
+  });
+
+  it('should not append extension given if extension exists on path ', () => {
+    expect(buildRequestPath({path: 'hello.json', extension: 'json'})).toEqual('hello.json');
   });
 });
